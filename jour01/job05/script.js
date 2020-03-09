618ab0e47c14b63d066ed16722c6766bb6c380d9
anthony.reboli <anthony.reboli@laplateforme.io>
2020-03-09 14:16:01 +0100

    jour semaines

---
+++ jour01/job05/script.js
@@ -0,0 +1,15 @@
+function afficherjourssemaines()
+{
+	var jourssemaines = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
+	 
+for (var i = 0; i < 7;)
+{
+   // Console.WriteLine(jours);
+   console.log(jourssemaines[i]);
+   i++
+}
+	
+	
+}
+
+afficherjourssemaines();
