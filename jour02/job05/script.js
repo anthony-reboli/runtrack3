c440bb25fc5bc71b079f322f98f6d8ce8e137f64
anthony.reboli <anthony.reboli@laplateforme.io>
2020-03-10 22:03:39 +0100

    Fini avec les pourcentage

--- jour02/job05/script.js
+++ jour02/job05/script.js
@@ -1,19 +1,25 @@
 window.addEventListener('scroll',function(event){
-	var scroll = this.scrollY;
-if (scroll <= 30)
+	
+ // prend la hauteur largeur de window pour detexter en %
+	var scrollTop = document.body.scrollTop,
+    windowHeight = window.innerHeight,
+    scrollHeight = document.body.scrollHeight;
+ 
+var scrollPercent = (scrollTop / (scrollHeight - windowHeight)) * 100;
+if (scrollPercent <= 30)
 	{
 
 		// document.body.style.backgroundColor = "blue";
 		document.getElementById("myP").className = "text";
 	}
 
-	 if (scroll >= 1400)
+	 if (scrollPercent >= 45)
 	{
 		// document.body.style.backgroundColor = "pink";
 		document.getElementById("myP").className = "text2";
 	}
 
-	if (scroll > 2500 )
+	if (scrollPercent > 50 )
 	{
 		// document.body.style.backgroundColor = "red";
 		document.getElementById("myP").className = "text3";
