02fad1f68e8c97798b9a588de9f923649f7bfa66
anthony.reboli <anthony.reboli@laplateforme.io>
2020-03-11 10:52:55 +0100

    show/hide

---
+++ jour03/job01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job 01 j3</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+<p id="el">Le $ est un bon élément monétaire</p>
+
+
+
+<button id="bouton">Cache</button>
+<button id="toto">montre</button>
+
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function(){
+
+	$("#el").hide();
+	$("#bouton").hide();
+
+	 $("#toto").click(function(){
+        $("#el").show();
+        $("#bouton").show()
+        $("#toto").hide();
+    });
+
+
+   $("#bouton").click(function(){
+        $("#el").hide();
+    });
+
+    
+    //Dès qu'on clique sur #b1, on applique show() au titre
+   
+
+  
+});
+
+
+
+
+		
+		
