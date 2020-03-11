cd17fa3c4e4ab79f75504f5719b75deb7693d80d
anthony.reboli <anthony.reboli@laplateforme.io>
2020-03-11 17:24:12 +0100

    Bon jeux fini =)

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>job 01 j3</title>
-
+<link rel="stylesheet" type="text/css" href="style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript" src="script.js"></script>
 </head>
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function(){
 
-
+var tab=[];
+var tab2=["PA1","PA2","PA3","PA4","PA5","PA6"];
  $("#bouton").click(function(){
 
     var parent = $("#melanger");
@@ -14,12 +15,65 @@ jQuery(document).ready(function(){
 
       $("img").click(function(){
    $(this).appendTo( "#ranger" );
-  });
-
-
-
+   // commence a calculer
+    var count = $("#ranger img").length;
+     console.log(count);
   
+    tab.push($(this).attr('id'));
+    console.log(tab);
+    console.log(tab2);
+
+var i = 1;
+ if (count == 6)
+ {
+    var win= true;
+
+
+while( i < 6 ){
+
+
+         if( tab[i] != tab2[i]){
+            win = false;
+                 
+            
+                                }
+                                i++;
+     
+
+}
+if (win)
+    {
+        var p = document.createElement('p');
+        var newelement = document.createTextNode("Vous avez gagné");
+        p.setAttribute("id", "win");
+        document.body.appendChild(p);
+        p.appendChild(newelement);
+    }
+
+    else {
+
+        var p = document.createElement('p');
+        var newelement = document.createTextNode("Vous avez perdu");
+        p.setAttribute("id", "perdu");
+        document.body.appendChild(p);
+        p.appendChild(newelement);
+    }
+
+} 
+
+
+      // if(count == 6)
+      //   {
+      //       console.log("tu a fini");
+      //   }
+
 });
+       
+
+
+
+});
+
 
 
 
